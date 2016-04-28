122babf38c3386405d49c0892bc23034ce4d4410
Arthur Moore <Arthur.Moore.git@cd-net.net>
2016-04-28 15:34:57 -0400

    Simplified the macro reader and writer
    Reads and writers are no longer batch processed, easing code readability.

--- extract_macros.py
+++ extract_macros.py
@@ -8,34 +8,22 @@ from FFXIV_settings_common import *
 
 def read_macro(in_file):
     macro={"data": []}
-    macro_sections=[]
     #There are exactly 18 sections per macro
-    for i in range(0,18):
-        macro_sections.append(read_section(in_file,0x73))
-    macro["name"] = macro_sections[0]['data']
-    macro["icon"] = macro_sections[1]['data']
-    macro["key"] = macro_sections[2]['data']
+    macro["name"] = read_section(in_file,0x73)['data']
+    macro["icon"] = read_section(in_file,0x73)['data']
+    macro["key"]  = read_section(in_file,0x73)['data']
     #Every macro has 15 lines
-    for i in range(3,18):
-        macro["data"].append(macro_sections[i]['data'])
+    for i in range(0,15):
+        macro["data"].append(read_section(in_file,0x73)['data'])
     return macro
 
 def write_macro(out_file,macro):
-    macro_sections=[]
-    for i in range(0,18):
-        macro_sections.append({})
-    macro_sections[0]['type'] = 'T'
-    macro_sections[0]['data'] = macro["name"]
-    macro_sections[1]['type'] = 'I'
-    macro_sections[1]['data'] = macro["icon"]
-    macro_sections[2]['type'] = 'K'
-    macro_sections[2]['data'] = macro["key"]
+    write_section(out_file,0x73,{'type':'T','data':macro["name"]})
+    write_section(out_file,0x73,{'type':'I','data':macro["icon"]})
+    write_section(out_file,0x73,{'type':'K','data':macro["key"]})
     #Every macro has 15 lines
-    for i in range(3,18):
-        macro_sections[i]['type'] = 'L'
-        macro_sections[i]['data'] = macro["data"][i-3]
-    for i in range(0,18):
-        write_section(out_file,0x73,macro_sections[i])
+    for i in range(0,15):
+        write_section(out_file,0x73,{'type':'L','data':macro["data"][i]})
 
 def print_macro(in_macro):
     print("Name: ",in_macro["name"])
