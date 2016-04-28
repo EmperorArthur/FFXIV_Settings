a2b6ba1e9a6c15fd68991a47c39ce694a6b422bf
Arthur Moore <Arthur.Moore.git@cd-net.net>
2016-04-28 14:53:37 -0400

    Switched Macro from a class based system to a data driven one
    Object oriented is nice, but it prevents easy use of some python modules

--- extract_macros.py
+++ extract_macros.py
@@ -6,50 +6,43 @@
 from __future__ import print_function
 from FFXIV_settings_common import *
 
-class macro:
-    def __init__(self, in_file = None):
-        self.name = ''
-        self.icon = 0
-        self.key = 0
-        self.lines = []
-        if(in_file):
-            self.read(in_file)
+def read_macro(in_file):
+    macro={"data": []}
+    macro_sections=[]
+    #There are exactly 18 sections per macro
+    for i in range(0,18):
+        macro_sections.append(section(0x73,in_file))
+    macro["name"] = macro_sections[0].data
+    macro["icon"] = macro_sections[1].data
+    macro["key"] = macro_sections[2].data
+    #Every macro has 15 lines
+    for i in range(3,18):
+        macro["data"].append(macro_sections[i].data)
+    return macro
 
-    def read(self,in_file):
-        macro_sections=[]
-        #There are exactly 18 sections per macro
-        for i in range(0,18):
-            macro_sections.append(section(0x73,in_file))
-        self.name = macro_sections[0].data
-        self.icon = macro_sections[1].data
-        self.key = macro_sections[2].data
-        #Every macro has 15 lines
-        for i in range(3,18):
-            self.lines.append(macro_sections[i].data)
-
-    def write(self,out_file):
-        macro_sections=[]
-        for i in range(0,18):
-            macro_sections.append(section(0x73))
-        macro_sections[0].type = 'T'
-        macro_sections[0].data = self.name
-        macro_sections[1].type = 'I'
-        macro_sections[1].data = self.icon
-        macro_sections[2].type = 'K'
-        macro_sections[2].data = self.key
-        #Every macro has 15 lines
-        for i in range(3,18):
-            macro_sections[i].type = 'L'
-            macro_sections[i].data = self.lines[i]
-        for i in range(0,18):
-            macro_sections[i].write(out_file)
+def write_macro(out_file,macro):
+    macro_sections=[]
+    for i in range(0,18):
+        macro_sections.append(section(0x73))
+    macro_sections[0].type = 'T'
+    macro_sections[0].data = macro["name"]
+    macro_sections[1].type = 'I'
+    macro_sections[1].data = macro["icon"]
+    macro_sections[2].type = 'K'
+    macro_sections[2].data = macro["key"]
+    #Every macro has 15 lines
+    for i in range(3,18):
+        macro_sections[i].type = 'L'
+        macro_sections[i].data = macro["data"][i-3]
+    for i in range(0,18):
+        macro_sections[i].write(out_file)
 
 def print_macro(in_macro):
-    print("Name: ",in_macro.name)
-    print("Icon: ",in_macro.icon)
-    print("Key:  ",in_macro.key)
+    print("Name: ",in_macro["name"])
+    print("Icon: ",in_macro["icon"])
+    print("Key:  ",in_macro["key"])
     print("Data (15 lines):")
-    for i in in_macro.lines:
+    for i in in_macro["data"]:
         #Sanitize the line to ascii only characters to prevent print from choking (For python3)
         #sanitized_line = i.encode('ascii','ignore').decode('ascii')
         sanitized_line = i
@@ -57,6 +50,8 @@ def print_macro(in_macro):
 
 in_file_name = "MACRO.DAT"
 in_file = open(in_file_name, "rb")
+out_file_name = "MACRO_check.DAT"
+out_file = open(out_file_name, "wb")
 
 #Confirm size matches in header
 check_header_size(in_file)
@@ -67,4 +62,7 @@ in_file.seek(0x11)
 
 #Print macros while there is still valid data
 while in_file.tell() < data_size:
-    print_macro(macro(in_file))
+    macro_in=read_macro(in_file)
+    if(macro_in["key"] != '000'):
+        print_macro(macro_in)
+    write_macro(out_file,macro_in)
